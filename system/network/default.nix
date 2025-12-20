babdc6e15465f0407f9e3088e29943dcef25bf59
Julien CLAISSE <julienclaisse@orange.fr>
2025-12-20 20:48:36 +0100

    🕊 Upgrade netbird to the latest package

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,6 +1,17 @@
-{ lib, config, options, ... }:
+{ lib, config, pkgs, options, ... }:
 let 
-  netbird-latest = ( import <nixpkgs-unstable> {} ).netbird;
+  netbird-latest = pkgs.netbird.overrideAttrs (oldAttrs: rec {
+    version = "0.60.8";
+    src = pkgs.fetchFromGitHub {
+      owner = "netbirdio";
+      repo = "netbird";
+      rev = "v${version}";
+      # hash = lib.fakeHash; 
+      hash = "sha256-XM4pUYimxbO3ZCmTPbg7dwDB3x2TnL9PUgbMfjHjxmo="; 
+    };
+    # vendorHash = lib.fakeHash;
+    vendorHash = "sha256-b3Wl9jsAdYC91JM/kDo4yIF05hqbivtrcn1aRuZzP3s=";
+  }); 
 in
 {
   imports = [
